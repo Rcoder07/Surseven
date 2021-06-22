7134cddf67964c09748c34ebd24c503b837f4321
Rcoder07 <86282615+Rcoder07@users.noreply.github.com>
2021-06-22 21:28:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 with open('config.json', 'r') as c:
     params= json.load(c) ["params"]
    
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@server/surseven website'   
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@server.site.com/surseven website'   
 db = SQLAlchemy(app)
 
 class Videoscompany(db.Model):
